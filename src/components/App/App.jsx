7778ed4fddb7c5ce68fd1682f16ebe4f3eee2581
uncle-Stas <stanislav.zavadskyi11@gmail.com>
2022-09-25 14:29:13 +0300

    finished setting React App

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <p>New REACT App</p>
+      </>
+    );
+  }
+}
+
+export default App;
